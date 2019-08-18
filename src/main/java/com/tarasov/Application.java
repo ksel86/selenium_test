6bb4ec1d79bdbd40724d578bf059ffeaa9bcc5d4
alex <atarasov.mail@gmial.com>
2019-08-18 14:26:44 +0100

    test

--- src/main/java/com/tarasov/Application.java
+++ src/main/java/com/tarasov/Application.java
@@ -1,17 +1,8 @@
 package com.tarasov;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.URL;
-
 public class Application {
     public static void main(String[] args) throws Exception {
 
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        WebDriver webDriver = new RemoteWebDriver(new URL("http://192.168.99.100:32769/wd/hub"), chromeOptions);
-        webDriver.get("https://www.youtube.com/");
+
     }
 }
---
+++ src/test/java/com/tarasov/MainTest.java
@@ -0,0 +1,22 @@
+package com.tarasov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void test() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+//        WebDriver webDriver = new RemoteWebDriver(new URL("http://192.168.99.100:32769/wd/hub"), chromeOptions);
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://www.youtube.com/");
+        assertEquals("80px", webDriver.findElement(By.id("logo-icon-container")).getCssValue("width"));
+    }
+}
